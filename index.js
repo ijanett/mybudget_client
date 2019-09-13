7a06a54f0afab17c505b11bf6161f7e2383d2e23
Irma Mejia <ijanettmejia@outlook.com>
2019-09-13 00:48:04 -0400

    Declare and assign budget container variables

---
+++ index.js
@@ -0,0 +1,14 @@
+const newBudgetContainer = document.querySelector("#new-budget-container")
+const budgetContainer = document.querySelector("#budget-container")
+
+
+// function renderUsers(json) {
+//     Object.keys(json).forEach(function(key) {
+//         const userObjs = json[key];
+//         userObjs.forEach(user => {
+//             userContainer.innerHTML += `
+//             <h3>${user.attributes.username}</h3>
+//             `
+//         })
+//     })
+// }
