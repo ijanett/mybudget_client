865cb1188c687b02653821d545548660b770464f
Irma Mejia <ijanettmejia@outlook.com>
2019-09-14 10:42:31 -0400

    Filter budgets and render income

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const newBudgetContainer = document.querySelector("#new-budget-container");
 const budgetContainer = document.querySelector("#budget-container");
 const subcategoryDropdown = document.getElementById("subcategory-dropdown");
-const incomeDescription = document.getElementById("income-description");
-const incomeAmount = document.getElementById("income-amount");
-const expenseDescription = document.getElementById("expense-description");
-const expenseAmount = document.getElementById("expense-amount")
+const incomeInputDescription = document.getElementById("income-description");
+const incomeInputAmount = document.getElementById("income-amount");
+const incomeSubmitBtn = document.querySelector("#income-submit-button");
+const incomeList = document.getElementById("income-list")
+const expenseInputDescription = document.getElementById("expense-description");
+const expenseInputAmount = document.getElementById("expense-amount")
+const expenseSubmitBtn = document.querySelector("#expense-submit-button");
 let expenseChart = document.getElementById("expense-chart").getContext("2d");
 let user;
 let budgets;
+let income;
+let expense;
 let subcategoryNames;
 let subcategories = [];
 
-// get subcategories and store in array
+
+function renderIncome(objArray) {
+    objArray.forEach(obj => {
+        incomeList.innerHTML += `
+            <li>${obj.attributes.description} $${obj.attributes.amount}</li>
+        `
+    })
+}
+
+// expense form submit
+expenseSubmitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+})
+
+// get subcategories and add to dropdown option values
+// store in array
 fetch("http://localhost:3000/subcategories")
     .then(res => res.json())
     .then(json => {
@@ -30,7 +50,11 @@ fetch("http://localhost:3000/users/1")
         user = json;
         budgets = json.included;
         console.log(user);
-        console.log(budgets);
+        income = budgets.filter(budget => {
+            return budget.attributes.category === "income"
+        })
+        console.log(income)
+        renderIncome(income);
         
         // budgetContainer.innerHTML += `
         // <h3>${user.data.attributes.username}</h3>
